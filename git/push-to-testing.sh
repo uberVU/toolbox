98585fc7eade6c20f4dd7ed0baf8edc1370496f4
Mihai Oprea <mishu@mishulica.com>
2014-03-03 17:41:32 +0200

    Added push-to-testing github script.
    We use a separate branch to push changes and test before going live. This action happens quite a lot; therefore we felt the need to automate all the necessary git commands.

---
+++ git/push-to-testing.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Delete and re-fetch testing branch
+# Merge current branch into testing and push testing
+# Re-checkout current branch
+
+# For ease of use alias this script into your bash or zsh config file 
+# (i.e. alias push_testing='~/.ubervu/thehole/uvscripts/github/push_in_testing.sh'
+
+# Make sure we fail on errors
+set -e
+
+CURRENT_BRANCH=`git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'`
+git branch -D testing
+git fetch origin testing:testing
+git checkout testing
+echo "Merging in " $CURRENT_BRANCH
+git merge $CURRENT_BRANCH
+
+# If for some reasons the merge wasn't successful and our set -e didn't stop 
+# the script, let's add one more check to make sure we don't push conflicts
+UNMERGED=`git diff --name-only --diff-filter=U`
+if [ -z "$UNMERGED" ]; then
+    git push origin testing
+    git checkout $CURRENT_BRANCH
+else
+    echo "ABORTING due tue unmerged paths"
+fi
